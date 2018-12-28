9893ea6a77d1c2dd5bde007101c9d4eb9c498e3a
duanzhiwei <armchannel_dzw@163.com>
2018-12-28 17:24:20 +0800

    update

--- recommendationSys/workflow/__init__.py
+++
@@ -1,3 +0,0 @@
-import sys
-
-sys.path.append('/Users/dzw/Desktop/RecommendSys/recommendation-master/workflow')
--- recommendationSys/workflow/test.py
+++
@@ -1,6 +0,0 @@
-data = "这是一个字符串"
-print("话费balabla %s" % data)
-
-print(type('cf'))
-
- 
